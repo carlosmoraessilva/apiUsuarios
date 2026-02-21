a0d53a1010ef3e8d1dbfa851c7ef1f12ee3aa86e
Carlos Moraes <carlosm1412@gmail.com>
2026-02-21 16:43:37 -0300

    Initial commit - API Usuarios .NET 8 - Criação do Projeto e Banco de Dados

---
+++ ApiUsuarios/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiUsuarios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiUsuarios.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+            
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+
+    }
+}
