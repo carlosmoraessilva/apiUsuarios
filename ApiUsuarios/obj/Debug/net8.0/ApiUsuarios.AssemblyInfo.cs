9f5f059bb04a0ddae8fda0bc02b03856a60d85c1
Carlos Moraes <carlosm1412@gmail.com>
2026-02-21 18:02:39 -0300

    Criando Response

--- ApiUsuarios/obj/Debug/net8.0/ApiUsuarios.AssemblyInfo.cs
+++ ApiUsuarios/obj/Debug/net8.0/ApiUsuarios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiUsuarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48efba4182d6f6772518508709c303a60473a19f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiUsuarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiUsuarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
